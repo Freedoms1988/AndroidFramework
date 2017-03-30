06b70546b597c50df6c820f5f75a66751e24c3bc
freedoms <zzt328697768@gmail.com>
2017-03-30 11:28:12 +0800

    集成网络请求组件

--- app/src/main/java/freedoms/androidframework/module/login/model/action/LoginAction.java
+++ app/src/main/java/freedoms/androidframework/module/login/model/action/LoginAction.java
@@ -2,9 +2,15 @@ package freedoms.androidframework.module.login.model.action;
 
 import android.os.Message;
 
+import java.io.IOException;
+
 import freedoms.androidframework.module.login.model.handler.ClearHandler;
 import freedoms.androidframework.module.login.model.handler.LoginHandler;
 import freedoms.androidframework.module.login.view.ILoginView;
+import freedoms.androidframework.tools.fokhttp.FOkHttpClient;
+import freedoms.androidframework.tools.fokhttp.FRequestCallBack;
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by freedoms on 2017/3/13.
@@ -22,8 +28,21 @@ public class LoginAction implements ILoginAction {
 
     @Override
     public void login(ILoginView iLoginView) {
-        new LoginThread(loginHandler,iLoginView.getUsername(),iLoginView.getPassword()).start();
-        System.out.println("启动线程");
+        FOkHttpClient.getInstance().getWithJSON("https://www.baidu.com",new FRequestCallBack(){
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Message msg=new Message();
+                msg.what=LoginHandler.LOGIN_FAILURE;
+                loginHandler.sendMessage(msg);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Message msg=new Message();
+                msg.what=LoginHandler.LOGIN_SUCCESS;
+                loginHandler.sendMessage(msg);
+            }
+        });
     }
 
     @Override
@@ -44,32 +63,32 @@ public class LoginAction implements ILoginAction {
 
 
 
-    class LoginThread extends Thread{
-
-        private String mUsername;
-        private String mPassword;
-        private LoginHandler loginHandler;
-
-        public LoginThread(LoginHandler loginHandler,String username,String password){
-            this.loginHandler=loginHandler;
-            this.mUsername=username;
-            this.mPassword=password;
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            try {
-                System.out.println("用户名："+this.mUsername+"  密码："+this.mPassword);
-                Message msg=new Message();
-                msg.what=LoginHandler.LOGIN_SUCCESS;
-                sleep(2000);
-                loginHandler.sendMessage(msg);
-                System.out.println("线程执行完成");
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-        }
-    }
+//    class LoginThread extends Thread{
+//
+//        private String mUsername;
+//        private String mPassword;
+//        private LoginHandler loginHandler;
+//
+//        public LoginThread(LoginHandler loginHandler,String username,String password){
+//            this.loginHandler=loginHandler;
+//            this.mUsername=username;
+//            this.mPassword=password;
+//        }
+//
+//        @Override
+//        public void run() {
+//            super.run();
+//            try {
+//                System.out.println("用户名："+this.mUsername+"  密码："+this.mPassword);
+//                Message msg=new Message();
+//                msg.what=LoginHandler.LOGIN_SUCCESS;
+//                sleep(2000);
+//                loginHandler.sendMessage(msg);
+//                System.out.println("线程执行完成");
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//
+//        }
+//    }
 }
